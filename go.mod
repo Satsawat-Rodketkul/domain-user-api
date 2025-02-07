6038c98e9971478a3f2f70464ebcba840ced2316
Satsawat <satsawatrodketkul@gmail.com>
2025-02-07 22:53:52 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Satsawat-Rodketkul/domain-user-api
+
+go 1.23.6
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ internal/service/login.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/Satsawat-Rodketkul/domain-user-api/internal/models"
+)
+
+func Login(request models.LoginRequest) (models.LoginResponse, error) {
+
+	return models.LoginResponse{}, errors.New("SYSTEM ERROR")
+}
