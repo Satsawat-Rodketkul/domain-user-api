440548d15f56b0eafa03f30730f932cbb8fd35bf
Satsawat <satsawatrodketkul@gmail.com>
2025-02-09 23:00:03 +0700

    add register service

---
+++ internal/service/register.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/Satsawat-Rodketkul/domain-user-api/internal/models"
+)
+
+func Register(models.RegisterRequest) error {
+
+	return errors.New("SYSTEM ERROR")
+}
